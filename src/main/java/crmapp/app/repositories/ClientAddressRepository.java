6ddc7a859ddaba5b40e8245ac3f63c13ffa12c6d
ryhan000 <rayhan1ahamed@gmail.com>
2018-05-02 23:59:30 +0600

    added  ClientAddressRepository

---
+++ src/main/java/crmapp/app/repositories/ClientAddressRepository.java
@@ -0,0 +1,16 @@
+package crmapp.app.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import crmapp.app.entities.ClientAddress;
+
+public interface ClientAddressRepository extends JpaRepository<ClientAddress, Integer> {
+
+	@Query("SELECT ca FROM ClientAddress ca WHERE ca.client.id = :clientId")
+	public List<ClientAddress> findAllClientAddressesByClientId(@Param("clientId") Integer clientId);
+
+}
