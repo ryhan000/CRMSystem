6b636bc1c56c8afc3812214e610306aff8f6444d
ryhan000 <rayhan1ahamed@gmail.com>
2018-05-02 23:57:21 +0600

    added  ClientAddress entitie

---
+++ src/main/java/crmapp/app/entities/ClientAddress.java
@@ -0,0 +1,71 @@
+package crmapp.app.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "client_address")
+@JsonIgnoreProperties(ignoreUnknown = true, 
+	value = { "hibernateLazyInitializer", "handler" })
+public class ClientAddress extends UrlBaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "client_id")
+	@JsonBackReference(value = "client-address")
+	private Client client;
+
+	@Column(name = "presentation", length = 255)
+	private String presentation;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "date_start")
+	private Date dateStart;
+
+	public ClientAddress() {
+	}
+
+	public ClientAddress(Client client, String presentation, Date dateStart) {
+		this.client = client;
+		this.presentation = presentation;
+		this.dateStart = dateStart;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public String getPresentation() {
+		return presentation;
+	}
+
+	public void setPresentation(String presentation) {
+		this.presentation = presentation;
+	}
+
+	public Date getDateStart() {
+		return dateStart;
+	}
+
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+
+}
