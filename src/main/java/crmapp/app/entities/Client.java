a237a4121c0ca0c2b3d1bd494ddb704fed04bd55
ryhan000 <rayhan1ahamed@gmail.com>
2018-05-22 03:39:48 +0600

    updated Client,ClientAccount

--- src/main/java/crmapp/app/entities/Client.java
+++ src/main/java/crmapp/app/entities/Client.java
@@ -1,42 +1,21 @@
 package crmapp.app.entities;
 
-import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "client")
-@JsonIgnoreProperties(ignoreUnknown = true, 
-	value = { "hibernateLazyInitializer", "handler" })
-public class Client extends UrlBaseEntity implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-	@Column(name = "title", length = 255)
-	private String title;
-
-	@Column(name = "alias", length = 100)
-	private String alias;
-
-	@Column(name = "edrpou", length = 12)
-	private String edrpou;
-
-	@Column(name = "inn", length = 15)
-	private String inn;
-
-	@Column(name = "vat_certificate", length = 20)
-	private String vatCertificate;
+@JsonIgnoreProperties(ignoreUnknown = true, value = { "hibernateLazyInitializer", "handler", "agreements", "addresses", "directors", "accounts" })
+public class Client extends AbstractCompany {
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "client", orphanRemoval = true)
 	@OrderBy("id ASC")
@@ -62,51 +41,11 @@ public class Client extends UrlBaseEntity implements Serializable {
 	}
 
 	public Client(String title, String alias, String edrpou, String inn, String vatCertificate) {
-		this.title = title;
-		this.alias = alias;
-		this.edrpou = edrpou;
-		this.inn = inn;
-		this.vatCertificate = vatCertificate;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAlias() {
-		return alias;
-	}
-
-	public void setAlias(String alias) {
-		this.alias = alias;
-	}
-
-	public String getEdrpou() {
-		return edrpou;
-	}
-
-	public void setEdrpou(String edrpou) {
-		this.edrpou = edrpou;
-	}
-
-	public String getInn() {
-		return inn;
-	}
-
-	public void setInn(String inn) {
-		this.inn = inn;
-	}
-
-	public String getVatCertificate() {
-		return vatCertificate;
-	}
-
-	public void setVatCertificate(String vatCertificate) {
-		this.vatCertificate = vatCertificate;
+		this.setTitle(title);
+		this.setAlias(alias);
+		this.setEdrpou(edrpou);
+		this.setInn(inn);
+		this.setVatCertificate(vatCertificate);
 	}
 
 	public Set<ClientAgreement> getAgreements() {
@@ -142,15 +81,11 @@ public class Client extends UrlBaseEntity implements Serializable {
 	}
 
 	@Override
-	@JsonIgnore
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("title: " + getTitle() + ", ");
-		builder.append("alias: " + getAlias() + ", ");
-		builder.append("edrpou: " + getEdrpou() + ", ");
-		builder.append("inn: " + getInn() + ", ");
-		builder.append("vatCertificate: " + getVatCertificate());
-		return builder.toString();
+		return new StringBuilder("Client [").append(super.toString()).append(", ")
+				.append("agreements=" + agreements.size()).append(", ").append("addresses=" + addresses.size())
+				.append(", ").append("directors=" + directors.size()).append(", ").append("accounts=" + accounts.size())
+				.append("]").toString();
 	}
 
 }
--- src/main/java/crmapp/app/entities/ClientAccount.java
+++ src/main/java/crmapp/app/entities/ClientAccount.java
@@ -1,16 +1,12 @@
 package crmapp.app.entities;
 
-import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -19,29 +15,19 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Table(name = "client_account")
 @JsonIgnoreProperties(ignoreUnknown = true, 
 	value = { "hibernateLazyInitializer", "handler" })
-public class ClientAccount extends UrlBaseEntity implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+public class ClientAccount extends AbstractAccount {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "client_id")
 	@JsonBackReference(value = "client-account")
 	private Client client;
 
-	@Column(name = "presentation", length = 255)
-	private String presentation;
-
-	@Temporal(TemporalType.DATE)
-	@Column(name = "date_start")
-	private Date dateStart;
-
 	public ClientAccount() {
 	}
 
 	public ClientAccount(Client client, String presentation, Date dateStart) {
+		super(presentation, dateStart);
 		this.client = client;
-		this.presentation = presentation;
-		this.dateStart = dateStart;
 	}
 
 	public Client getClient() {
@@ -52,20 +38,18 @@ public class ClientAccount extends UrlBaseEntity implements Serializable {
 		this.client = client;
 	}
 
-	public String getPresentation() {
-		return presentation;
+	@Override
+	public String getUrl() {
+		return client.getUrl() + "/accounts/" + this.getId();
 	}
 
-	public void setPresentation(String presentation) {
-		this.presentation = presentation;
-	}
-
-	public Date getDateStart() {
-		return dateStart;
-	}
-
-	public void setDateStart(Date dateStart) {
-		this.dateStart = dateStart;
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ClientAccount [");
+		builder.append(super.toString()).append(", ");
+		builder.append("client=" + client).append("]");
+		return builder.toString();
 	}
 
 }
