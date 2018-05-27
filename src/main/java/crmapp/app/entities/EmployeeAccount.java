9b6f91b00b8aec22d514bd7254ef98e24a81653f
ryhan000 <rayhan1ahamed@gmail.com>
2018-05-28 01:20:53 +0600

    updated EmployeeAccount,EmployeeAddress

--- src/main/java/crmapp/app/entities/EmployeeAccount.java
+++ src/main/java/crmapp/app/entities/EmployeeAccount.java
@@ -1,16 +1,13 @@
 package crmapp.app.entities;
 
-import java.io.Serializable;
+
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -19,29 +16,21 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Table(name = "employee_account")
 @JsonIgnoreProperties(ignoreUnknown = true, 
 	value = { "hibernateLazyInitializer", "handler" })
-public class EmployeeAccount extends UrlBaseEntity implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+public class EmployeeAccount extends AbstractAccount {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "employee_id")
 	@JsonBackReference(value = "employee-account")
 	private Employee employee;
 
-	@Column(name = "presentation", length = 255)
-	private String presentation;
-
-	@Temporal(TemporalType.DATE)
-	@Column(name = "date_start")
-	private Date dateStart;
 
 	public EmployeeAccount() {
 	}
 
 	public EmployeeAccount(Employee employee, String presentation, Date dateStart) {
 		this.employee = employee;
-		this.presentation = presentation;
-		this.dateStart = dateStart;
+		this.setPresentation(presentation);
+		this.setDateStart(dateStart);
 	}
 
 	public Employee getEmployee() {
@@ -52,20 +41,18 @@ public class EmployeeAccount extends UrlBaseEntity implements Serializable {
 		this.employee = employee;
 	}
 
-	public String getPresentation() {
-		return presentation;
+	@Override
+	public String getUrl() {
+		return employee.getUrl() + "/accounts/" + this.getId();
 	}
 
-	public void setPresentation(String presentation) {
-		this.presentation = presentation;
-	}
-
-	public Date getDateStart() {
-		return dateStart;
-	}
-
-	public void setDateStart(Date dateStart) {
-		this.dateStart = dateStart;
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("EmployeeAccount [");
+		builder.append(super.toString()).append(", ");
+		builder.append("employee=" + employee).append(", ");
+		return builder.toString();
 	}
 
 }
--- src/main/java/crmapp/app/entities/EmployeeAddress.java
+++ src/main/java/crmapp/app/entities/EmployeeAddress.java
@@ -1,16 +1,12 @@
 package crmapp.app.entities;
 
-import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -19,29 +15,20 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Table(name = "employee_address")
 @JsonIgnoreProperties(ignoreUnknown = true, 
 	value = { "hibernateLazyInitializer", "handler" })
-public class EmployeeAddress extends UrlBaseEntity implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+public class EmployeeAddress extends AbstractAddress {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "employee_id")
 	@JsonBackReference(value = "employee-address")
 	private Employee employee;
 
-	@Column(name = "presentation", length = 255)
-	private String presentation;
-
-	@Temporal(TemporalType.DATE)
-	@Column(name = "date_start")
-	private Date dateStart;
-
 	public EmployeeAddress() {
 	}
 
 	public EmployeeAddress(Employee employee, String presentation, Date dateStart) {
 		this.employee = employee;
-		this.presentation = presentation;
-		this.dateStart = dateStart;
+		this.setPresentation(presentation);
+		this.setDateStart(dateStart);
 	}
 
 	public Employee getEmployee() {
@@ -52,20 +39,18 @@ public class EmployeeAddress extends UrlBaseEntity implements Serializable {
 		this.employee = employee;
 	}
 
-	public String getPresentation() {
-		return presentation;
+	@Override
+	public String getUrl() {
+		return employee.getUrl() + "/addresses/" + this.getId();
 	}
 
-	public void setPresentation(String presentation) {
-		this.presentation = presentation;
-	}
-
-	public Date getDateStart() {
-		return dateStart;
-	}
-
-	public void setDateStart(Date dateStart) {
-		this.dateStart = dateStart;
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("EmployeeAddress [");
+		builder.append(super.toString()).append(", ");
+		builder.append("employee=" + employee).append("]");
+		return builder.toString();
 	}
 
 }
