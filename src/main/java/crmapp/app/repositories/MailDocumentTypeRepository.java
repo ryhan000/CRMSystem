995adae06029157361fc15262f264ad38a228496
ryhan000 <rayhan1ahamed@gmail.com>
2018-05-30 04:10:25 +0600

    added  MailOutputRepository, MailDocumentTypeRepository

---
+++ src/main/java/crmapp/app/repositories/MailDocumentTypeRepository.java
@@ -0,0 +1,8 @@
+package crmapp.app.repositories;
+
+
+import crmapp.app.entities.MailDocumentType;
+
+public interface MailDocumentTypeRepository extends BaseRepository<MailDocumentType, Integer> {
+
+}
---
+++ src/main/java/crmapp/app/repositories/MailOutputRepository.java
@@ -0,0 +1,8 @@
+package crmapp.app.repositories;
+
+
+import crmapp.app.entities.MailOutput;
+
+public interface MailOutputRepository extends BaseRepository<MailOutput, Integer> {
+
+}
