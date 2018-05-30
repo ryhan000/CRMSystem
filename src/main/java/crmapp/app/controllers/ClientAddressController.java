cb3d18753276d3cab725dfd6c3f0b920d90270ac
ryhan000 <rayhan1ahamed@gmail.com>
2018-05-30 15:30:35 +0600

    updated  ClientAgreementController, ClientAddressController

--- src/main/java/crmapp/app/controllers/ClientAddressController.java
+++ src/main/java/crmapp/app/controllers/ClientAddressController.java
@@ -38,57 +38,67 @@ public class ClientAddressController extends BaseController {
 
 	@GetMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<List<ClientAddress>> getAllClientAddresses(@PathVariable("clientId") Integer clientId) {
+		logger.info(LOG_ENTER_METHOD + "getAllClientAddresses()" + LOG_CLOSE);
 		List<ClientAddress> addresses = addressRepository.findAllClientAddressesByClientId(clientId);
 		if (addresses.size() == 0) {
+			logger.info(LOG_ERROR + "ClientAddresses were not found" + LOG_CLOSE);
 			return new ResponseEntity<List<ClientAddress>>(HttpStatus.NO_CONTENT);
 		}
+		logger.info(LOG_TEXT + "Count of ClientAddresses: " + addresses.size() + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getAllClientAddresses()" + LOG_CLOSE);
 		return new ResponseEntity<List<ClientAddress>>(addresses, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<ClientAddress> getClientAddressById(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "getClientAddressById()" + LOG_CLOSE);
 		ClientAddress address = addressRepository.findOne(id);
 		if (address == null) {
+			logger.info(LOG_ERROR + "ClientAccount with ID=" + id + "wasn't found" + LOG_CLOSE);
 			return new ResponseEntity<ClientAddress>(address, HttpStatus.NOT_FOUND);
 		}
+		logger.info(LOG_OUT_OF_METHOD + "getClientAddressById()" + LOG_CLOSE);
 		return new ResponseEntity<ClientAddress>(address, HttpStatus.OK);
 	}
 
 	@PostMapping(value = "", headers = HEADER_JSON)
 	public ResponseEntity<ClientAddress> addClientAddress(@PathVariable("clientId") int clientId,
 			@RequestBody ClientAddress address) {
-		logger.info("<==/////////// Entering to the addClientAddress() method ... ///////////==>");
+		logger.info(LOG_ENTER_METHOD + "addClientAddress()" + LOG_CLOSE);
 		Client client = clientRepository.findOne(clientId);
-		logger.info("<==/////////// Getting the client with ID = " + client.getId() + " ///////////==>");
+		logger.info(LOG_TEXT + "Obtained Client with ID = " + client.getId() + LOG_CLOSE);
 		address.setClient(client);
 		address.setVersion(0);
 		address = addressRepository.save(address);
-		logger.info("<==/////////// Saving of ClientAddress with ID = " + address.getId()
-				+ " was successfull ... ///////////==>");
+		logger.info(LOG_TEXT + "ClientAddress added with ID=" + address.getId() + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "addClientAddress()" + LOG_CLOSE);
 		return new ResponseEntity<ClientAddress>(address, header, HttpStatus.CREATED);
 	}
 	
 	@PutMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<ClientAddress> updateClientAddress(@PathVariable("clientId") int clientId, 
 			@RequestBody ClientAddress address) {
-		logger.info("<==/////////// Entering to the updateClientAddress() method ... ///////////==>");
+		logger.info(LOG_ENTER_METHOD + "updateClientAddress()" + LOG_CLOSE);
 		Client client = clientRepository.findOne(clientId);
 		address.setClient(client);
-		logger.info("<==/////////// Client is setted to " + client + "///////////==>");
+		logger.info(LOG_TEXT + "Client is setted to " + client + LOG_CLOSE);
 		int actualVersionNumber = addressRepository.getOne(address.getId()).getVersion();
 		address.setVersion(actualVersionNumber);
-		logger.info("<==/////////// Printing address: " + address + "///////////==>");
 		address = addressRepository.save(address);
+		logger.info(LOG_TEXT + "ClientAddress was updated: " + address + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "updateClientAddress()" + LOG_CLOSE);
 		return new ResponseEntity<ClientAddress>(address, header, HttpStatus.OK);
 	}
 	
 	@DeleteMapping(value = "/{id}", headers = HEADER_JSON)
 	public ResponseEntity<Void> deleteClientAddress(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "deleteClientAddress()" + LOG_CLOSE);
 		addressRepository.delete(id);
-		logger.info("<==/////////// ClientAddress  with ID = " + id + " was deleted successfully!!! ///////////==>");
+		logger.info(LOG_TEXT + "ClientAccount with ID=" + id + " was deleted" + LOG_CLOSE);
 		HttpHeaders header = new HttpHeaders();
+		logger.info(LOG_OUT_OF_METHOD + "deleteClientAddress()" + LOG_CLOSE);
 		return new ResponseEntity<Void>(header, HttpStatus.NO_CONTENT);
 	}
 
--- src/main/java/crmapp/app/controllers/ClientAgreementController.java
+++ src/main/java/crmapp/app/controllers/ClientAgreementController.java
@@ -1,7 +1,6 @@
 package crmapp.app.controllers;
 
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import crmapp.app.entities.ClientAgreement;
 import crmapp.app.repositories.ClientAgreementRepository;
 
@@ -34,58 +32,73 @@ public class ClientAgreementController extends BaseController {
 
 	@GetMapping(value = "/agreements", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<ClientAgreement>> getAllAgreements() {
-		logger.info("<==/////////// Entering to the getAllAgreements() method ... ///////////==>");
+		logger.info(LOG_ENTER_METHOD + "getAllAgreements()" + LOG_CLOSE);
 		List<ClientAgreement> agreements = agreementRepository.findAll();
-		logger.info("<==/////////// Printing agreements: " + agreements + " ///////////==>");
 		if (agreements.size() == 0) {
+			logger.info(LOG_ERROR + "ClientAgreements were not found" + LOG_CLOSE);
 			return new ResponseEntity<List<ClientAgreement>>(HttpStatus.NO_CONTENT);
 		}
+		logger.info(LOG_TEXT + "Count of ClientAgreements: " + agreements.size() + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getAllAgreements()" + LOG_CLOSE);
 		return new ResponseEntity<List<ClientAgreement>>(agreements, HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/clients/{clientId}/agreements", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<ClientAgreement>> getAllAgreementsByClientId(@PathVariable("clientId") int clientId) {
-		logger.info("<==/////////// Entering to the getAllAgreementsByClientId() method ... ///////////==>");
+		logger.info(LOG_ENTER_METHOD + "getAllAgreementsByClientId()" + LOG_CLOSE);
 		List<ClientAgreement> agreements = agreementRepository.findAllAgreementsByClientId(clientId);
-		logger.info("<==/////////// Printing agreements: " + agreements + " ///////////==>");
 		if (agreements.size() == 0) {
+			logger.info(LOG_ERROR + "ClientAgreements by clientIdwere not found" + LOG_CLOSE);
 			return new ResponseEntity<List<ClientAgreement>>(HttpStatus.NO_CONTENT);
 		}
+		logger.info(LOG_TEXT + "Count of ClientAgreements by clientId: " + agreements.size() + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getAllAgreementsByClientId()" + LOG_CLOSE);
 		return new ResponseEntity<List<ClientAgreement>>(agreements, HttpStatus.OK);
 	}
 	
 	@GetMapping(value = { "/agreements/{id}", "/clients/{clientId}/agreements/{id}" }, headers = HEADER_JSON)
 	public ResponseEntity<ClientAgreement> getAgreementById(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "getAgreementById()" + LOG_CLOSE);
 		ClientAgreement agreement = agreementRepository.findOne(id);
 		if (agreement == null) {
+			logger.info(LOG_ERROR + "ClientAgreement with ID=" + id + "wasn't found" + LOG_CLOSE);
 			return new ResponseEntity<ClientAgreement>(agreement, HttpStatus.NOT_FOUND);
 		}
+		logger.info(LOG_TEXT + "ClientAgreement with ID=" + id + " was found: " + agreement + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "getAgreementById()" + LOG_CLOSE);
 		return new ResponseEntity<ClientAgreement>(agreement, HttpStatus.OK);
 	}
-	
+
 	@PostMapping(value = "/agreements", headers = HEADER_JSON)
-	public ResponseEntity<Void> addAgreement(@RequestBody ClientAgreement agreement) {
+	public ResponseEntity<ClientAgreement> addAgreement(@RequestBody ClientAgreement agreement) {
+		logger.info(LOG_ENTER_METHOD + "addAgreement()" + LOG_CLOSE);
+		agreement.setVersion(0);
 		agreement = agreementRepository.save(agreement);
-		HttpHeaders header = new HttpHeaders();
-		return new ResponseEntity<Void>(header, HttpStatus.CREATED);
+		logger.info(LOG_TEXT + "ClientAgreement added with ID=" + agreement.getId() + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "addAgreement()" + LOG_CLOSE);
+		return new ResponseEntity<ClientAgreement>(new HttpHeaders(), HttpStatus.CREATED);
 	}
 
 	@PutMapping(value = "/agreements/{id}", headers = HEADER_JSON)
-	public ResponseEntity<Void> updateAgreement(@PathVariable(PARAM_ID) int id, @RequestBody ClientAgreement agreement) {
+	public ResponseEntity<ClientAgreement> updateAgreement(@PathVariable(PARAM_ID) int id,
+			@RequestBody ClientAgreement agreement) {
+		logger.info(LOG_ENTER_METHOD + "updateAgreement()" + LOG_CLOSE);
 		agreement.setId(id);
-		agreement.setVersion(agreementRepository.getOne(id).getVersion());
+		int actualVersionNumber = agreementRepository.getOne(id).getVersion();
+		agreement.setVersion(actualVersionNumber);
 		agreement = agreementRepository.save(agreement);
-		HttpHeaders header = new HttpHeaders();
-		return new ResponseEntity<Void>(header, HttpStatus.OK);
+		logger.info(LOG_TEXT + "ClientAgreement with ID=" + id + " was updated: " + agreement + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "updateAgreement()" + LOG_CLOSE);
+		return new ResponseEntity<ClientAgreement>(new HttpHeaders(), HttpStatus.OK);
 	}
 
 	@DeleteMapping(value = { "/agreements/{id}", "/clients/{clientId}/agreements/{id}" }, headers = HEADER_JSON)
-	public ResponseEntity<Void> deleteAgreement(@PathVariable(PARAM_ID) int id, @RequestBody ClientAgreement agreement) {
-		agreement.setId(id);
-		agreement.setVersion(agreementRepository.getOne(id).getVersion());
-		agreementRepository.delete(agreement);
-		HttpHeaders header = new HttpHeaders();
-		return new ResponseEntity<Void>(header, HttpStatus.NO_CONTENT);
+	public ResponseEntity<Void> deleteAgreement(@PathVariable(PARAM_ID) int id) {
+		logger.info(LOG_ENTER_METHOD + "deleteAgreement()" + LOG_CLOSE);
+		agreementRepository.delete(id);
+		logger.info(LOG_TEXT + "ClientAgreement with ID=" + id + " was deleted" + LOG_CLOSE);
+		logger.info(LOG_OUT_OF_METHOD + "deleteAgreement()" + LOG_CLOSE);
+		return new ResponseEntity<Void>(new HttpHeaders(), HttpStatus.NO_CONTENT);
 	}
 
 }
